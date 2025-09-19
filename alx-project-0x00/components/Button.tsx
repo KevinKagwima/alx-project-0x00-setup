962e72f517a6d795db36ec24a14c17ba2f5c4b4e
KevinKagwima <KevinKagwima+github1@gmail.com>
2025-09-19 09:33:46 +0300

    Updated files

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ styles, title }) => {
+  return (
+    <>
+      <button className={styles}>{title}</button>
+    </>
+  );
+};
+
+export default Button;
