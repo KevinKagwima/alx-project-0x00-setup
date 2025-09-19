667b07619037476539f99dc70c706c5393c24d03
KevinKagwima <KevinKagwima+github1@gmail.com>
2025-09-19 09:33:34 +0300

    Updated files

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Button";
 import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
@@ -5,6 +6,11 @@ const Landing: React.FC = () => {
     <div className="p-4 bg-amber-50">
       <h1 className="text-black font-bold">This is the landing page</h1>
       <Card />
+      <div className="flex gap-2 mt-2">
+        <Button title="First Button" styles="bg-red-600 p-2 cursor-pointer" />
+        <Button title="Second Button" styles="bg-blue-600 p-2 cursor-pointer" />
+        <Button title="Third Button" styles="bg-green-600 p-2 cursor-pointer" />
+      </div>
     </div>
   );
 };
