cda8121bbdae6523dc82988c76cf991bfad06b41
KevinKagwima <KevinKagwima+github1@gmail.com>
2025-09-19 09:20:42 +0300

    Updated files

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -3,6 +3,7 @@ import Card from "@/components/Card";
 const Landing: React.FC = () => {
   return (
     <div className="p-4 bg-amber-50">
+      <h1 className="text-black font-bold">This is the landing page</h1>
       <Card />
     </div>
   );
